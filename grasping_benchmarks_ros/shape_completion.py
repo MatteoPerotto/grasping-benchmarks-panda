419a0919504165ef14a3ab262c0a85ad69ab827d
MatteoPerotto <perotto.matteo@gmail.com>
2022-12-01 15:18:48 +0100

    Fixed o3d ModuleNotFound

--- grasping_benchmarks_ros/shape_completion.py
+++ grasping_benchmarks_ros/shape_completion.py
@@ -2,9 +2,7 @@ import numpy as np
 import torch
 from pathlib import Path
 
-from open3d.cuda.pybind.geometry import PointCloud
-from open3d.cuda.pybind.utility import Vector3dVector
-from open3d.visualization import draw
+import open3d as o3d 
 
 from pcr.model import PCRNetwork as Model
 from pcr.utils import Normalize, Denormalize
@@ -44,9 +42,9 @@ def complete_point_cloud(partial_pc):
     complete = Denormalize(Config.Processing)(complete, ctx)
     partial = Denormalize(Config.Processing)(partial, ctx)
 
-    draw([
-        PointCloud(points=Vector3dVector(partial)).paint_uniform_color([0, 1, 1]),
-        PointCloud(points=Vector3dVector(complete)).paint_uniform_color([1, 0, 1]),
+    o3d.visualization.draw([
+        o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(partial)).paint_uniform_color([0, 1, 1]),
+        o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(complete)).paint_uniform_color([1, 0, 1]),
         ],
         bg_color=(0,0,0,0),
         show_skybox=False)
@@ -78,9 +76,9 @@ def complete_point_cloud(partial_pc):
 #     partial = Denormalize(Config.Processing)(partial, ctx)
 
 #     if visualize:
-#         draw([
-#               PointCloud(points=Vector3dVector(partial)).paint_uniform_color([0, 0, 1]),
-#               PointCloud(points=Vector3dVector(complete)).paint_uniform_color([0, 1, 1]),
+#         o3d.visualization.draw([
+#               o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(partial)).paint_uniform_color([0, 0, 1]),
+#               o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(complete)).paint_uniform_color([0, 1, 1]),
 #               ])
 
 #     print(complete)
